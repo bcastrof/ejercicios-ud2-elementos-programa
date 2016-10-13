b9abc1ea2cd99238a06d5783c6cdbde1fb37d210
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2016-10-13 19:14:17 +0200

    Versión del ejercicio con String en vez de char

---
+++ 10_temperatura/TemperaturasString/src/com/jaureguialzo/Main.java
@@ -0,0 +1,48 @@
+package com.jaureguialzo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Escribe la temperatura en ºC: ");
+        double temperatura = Double.parseDouble(br.readLine());
+
+        System.out.print("Elige (F|K): ");
+        String opcion = br.readLine();
+
+        if (opcion.equals("F") || opcion.equals("f")) {
+            double fahrenheit = temperatura * 9 / 5 + 32;
+            System.out.println("La temperatura en ºF es: " + fahrenheit);
+        } else if (opcion.equalsIgnoreCase("K")) {
+            double kelvin = temperatura + 273.15;
+            System.out.println("La temperatura en ºK es: " + kelvin);
+        } else {
+            System.out.println("ERROR!!!");
+        }
+
+/*
+        switch (opcion) {
+            case "F":
+            case "f":
+                double fahrenheit = temperatura * 9 / 5 + 32;
+                System.out.println("La temperatura en ºF es: " + fahrenheit);
+                break;
+            case "K":
+            case "k":
+                double kelvin = temperatura + 273.15;
+                System.out.println("La temperatura en ºK es: " + kelvin);
+                break;
+            default:
+                System.out.println("ERROR!!!");
+                break;
+        }
+*/
+
+    }
+}
